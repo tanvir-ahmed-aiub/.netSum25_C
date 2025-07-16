76772667b7200230d3b50dadc1cf664b8ef9daa3
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-07-16 16:02:01 +0600

    IntroMVC,View

---
+++ IntroMVC/IntroMVC/Controllers/PortfolioController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
---
+++ IntroMVC/IntroMVC/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using IntroMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace IntroMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            /*Student s1 = new Student() {
+                Id = 1,
+                Name = "S1",
+                Email = "e.a@g.c",
+                Address = "Dhaka"
+            };
+            Student s2 = new Student()
+            {
+                Id = 2,
+                Name = "S2",
+                Email = "e.a@g.c",
+                Address = "Dhaka"
+            };*/
+            List<Student> students = new List<Student>();
+            for (int i = 1; i <= 20; i++) {
+                Student s = new Student()
+                {
+                    Id = i,
+                    Name = "S"+i,
+                    Email = i+"e.a@g.c",
+                    Address = "Dhaka"
+                };
+                students.Add(s);
+            }
+            
+            
+
+            return View(students);
+        }
+        public ActionResult Create()
+        {
+            TempData["Msg"] = "Redirected from Student Create";
+            return RedirectToAction("Index","Home");
+            
+        }
+    }
+}
