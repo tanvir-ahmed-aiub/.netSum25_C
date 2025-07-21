7a65511641329c3b48e595d8cf9fd74ad5cfaace
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-07-21 16:02:17 +0600

    Form validation/submission

---
+++ FormSubmission/FormSubmission/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using FormSubmission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create() { 
+            return View(new Student());
+        }
+        [HttpPost]
+
+        public ActionResult Create(Student s) {
+            if (ModelState.IsValid) {
+                return RedirectToAction("Index","Home");
+            }
+            return View(s);
+        }
+        //[HttpPost]
+        //public ActionResult Create(string Name, string Id, string Address) {
+        //    ViewBag.Name = Name;
+        //    ViewBag.Id = Id;
+        //    ViewBag.Address = Address;
+        //    return View();
+
+        //}
+        //[HttpPost]
+        //public ActionResult Create(FormCollection obj) {
+
+        //    ViewBag.Name = obj["Name"];
+        //    ViewBag.Id = obj["Id"];
+        //    ViewBag.Email = obj["Email"];
+        //    ViewBag.Address = obj["Address"];
+        //    return View();
+        //}
+
+        //public ActionResult Create() {
+        //    string n = Request["Name"];
+        //    return View();
+        //}
+    }
+}
---
+++ FormSubmission/FormSubmission/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormSubmission.Models
+{
+    public class Student
+    {
+        [Required]
+        [Range(1,40)]
+        public int? Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(200,MinimumLength =20)]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Email Needed")]
+        [EmailAddress]
+        
+        public string Email { get; set; }
+    }
+}
